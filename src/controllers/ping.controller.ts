1af6196b2b4f4226a59a4ae6a5bc1af03f37471e
Sreetam Ganguly <sreetamganguly@Sreetams-MacBook-Air.local>
2026-09-21 17:40:49 +0530

    Add minimal LoopBack 4 app (default welcome page, /explorer, /ping)

---
+++ src/controllers/ping.controller.ts
@@ -0,0 +1,44 @@
+import {get, Request, ResponseObject, RestBindings} from '@loopback/rest';
+import {inject} from '@loopback/core';
+
+const PING_RESPONSE: ResponseObject = {
+  description: 'Ping Response',
+  content: {
+    'application/json': {
+      schema: {
+        type: 'object',
+        title: 'PingResponse',
+        properties: {
+          greeting: {type: 'string'},
+          date: {type: 'string'},
+          url: {type: 'string'},
+          headers: {
+            type: 'object',
+            properties: {
+              'Content-Type': {type: 'string'},
+            },
+            additionalProperties: true,
+          },
+        },
+      },
+    },
+  },
+};
+
+export class PingController {
+  constructor(@inject(RestBindings.Http.REQUEST) private req: Request) {}
+
+  @get('/ping', {
+    responses: {
+      '200': PING_RESPONSE,
+    },
+  })
+  ping(): object {
+    return {
+      greeting: 'Hello from LoopBack',
+      date: new Date(),
+      url: this.req.url,
+      headers: Object.assign({}, this.req.headers),
+    };
+  }
+}
